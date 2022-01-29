1b5270c9e8a677a74d2e98c207ca93b8b8cf4492
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2022-01-29 15:03:53 +0100

    created login endpoint

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
+var jwt = require('jsonwebtoken');
 
-router.get('/', async function (req, res) {
+const MOCK_USER = {
+  email: 'admin@admin.com',
+  password: '12345'
+}
+
+router.post('/', async function (req, res) {
   try {
-    console.log('entra login');
+    const { email, password } = req.body;
+    if (MOCK_USER.email === email && MOCK_USER.password === password) {
+      jwt.sign(
+        { user: MOCK_USER },
+        'lalocuranuncatuvomaestro',
+        { expiresIn: '1d' },
+        (err, token) => {
+          if (err) {
+            res.json({ message: 'something went wrong' });
+            return;
+          }
+          res.json({
+            message: 'usuario logueado correctamente',
+            success: true,
+            user: { ...MOCK_USER, token },
+          });
+        });
+      return;
+    }
+    return res.json({ success: false, message: 'No existe un usuario con ese email' });
   } catch (error) {
     res.json(error);
   }
